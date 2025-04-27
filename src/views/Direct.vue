19bc6df9f7e57a69558511c54855645ab0fedbf8
klaushofrichter <klaus@klaushofrichter.net>
2025-04-26 20:02:24 -0500

    Refactor logout buttons in App.vue for improved layout; add immediate logout functionality; update placeholder in Direct.vue for clarity; remove unnecessary logging in auth.js for cleaner code.

--- src/views/Direct.vue
+++ src/views/Direct.vue
@@ -33,7 +33,7 @@
                     v-model="baseUrl"
                     type="text"
                     required
-                    placeholder="e.g. api.eagleeyenetworks.com"
+                    placeholder="e.g. api.c021.eagleeyenetworks.com"
                     class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
                   />
                 </div>
