89990d2a866a8b33b96309f5d4f8c42f8653c502
Klaus Hofrichter <klaus@klaushofrichter.net>
2025-04-29 13:15:28 -0500

    Update vite.config.js
    added base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/een-login/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
